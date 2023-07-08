4240e378aebd602b6a976d77e715c73da6e30548
Cajo ter Braak <cajo.terbraak@wur.nl>
2023-07-08 16:11:41 +0200

    Update demo3.r
    GDFscore -> GDFscores

--- demo/demo3.r
+++ demo/demo3.r
@@ -5,8 +5,8 @@ data("JanMayenBirds")
 #get the measurements in the generalized discriminant function (GFD) from the new data
 newdata <- as.matrix(JanMayenBirds[,  c("HB","BD2","TL","CL")])
 # combine the measurements using the coefficients of the GDF
-GDFscore <- newdata%*% result$GDF[,2]
-attr(GDFscore,which = "classnames") <- result$classnames
+GDFscores <- newdata%*% result$GDF[,2]
+attr(GDFscores,which = "classnames") <- result$classnames
 # note the attribute classnames with the names to be used in the printout
 # for first and second level of the factor sex
 # Calculate the cutpoint using unmix instead of predict.gendis
